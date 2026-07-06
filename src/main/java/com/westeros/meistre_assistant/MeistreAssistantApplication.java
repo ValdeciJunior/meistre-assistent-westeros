1cf14eb736eec2dbc4085aa4bd5cd4bf84f0477e
Valdeci Rolim Junior <rolim.valdeci.jr@gmail.com>
2026-07-06 15:46:09 -0300

    criacao do projeto

---
+++ src/main/java/com/westeros/meistre_assistant/MeistreAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.westeros.meistre_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeistreAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeistreAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/westeros/meistre_assistant/MeistreAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.westeros.meistre_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeistreAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
